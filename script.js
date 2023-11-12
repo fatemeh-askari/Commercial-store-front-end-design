3c50bc12878cda60c8d6ca30f72d18c48ccf5f49
fatemeh <fatemeh.askari.edu@gmail.com>
2023-11-12 23:37:24 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,138 @@
+// copy menu for mobile
+function copyMenu() {
+    // copy inside .dpt-cat to departments
+    var dptCategory = document.querySelector('.dpt-cat');
+    var dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    // copy inside nav to nav
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    // copy .header-top .wrapper to .thetop-nav
+    var topNav = document.querySelector('.header-top .wrapper');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
+
+}
+copyMenu();
+
+
+
+// Show mobile menu
+const menuButton = document.querySelector('.trigger');
+const closeButton = document.querySelector('.t-close');
+const addclass = document.querySelector('.site');
+
+menuButton.addEventListener('click', function() {
+    addclass.classList.toggle('showmenu');
+});
+
+closeButton.addEventListener('click', function() {
+    addclass.classList.remove('showmenu');
+});
+
+
+// Show sub menu on mobile
+document.addEventListener('DOMContentLoaded', function () {
+    const submenu = document.querySelectorAll('.has-child .icon-small');
+    submenu.forEach((menu) => menu.addEventListener('click', toggle));
+    
+    function toggle(e) {
+        e.preventDefault();
+        submenu.forEach((item) => {
+            if (item !== this) {
+                const parent = item.closest('.has-child');
+                parent.classList.remove('expand');
+            }
+        });
+
+        const parent = this.closest('.has-child');
+        parent.classList.toggle('expand');
+    }
+});
+
+
+
+//slider
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+});
+
+
+//show search
+const searchButton = document.querySelector('.t-search'),
+    tClose = document.querySelector('.search-close'),
+    showClass = document.querySelector('.site');
+
+searchButton.addEventListener('click', function() {
+    showClass.classList.toggle('showsearch')
+})
+tClose.addEventListener('click', function() {
+    showClass.classList.remove('showsearch')
+})
+
+
+
+// show dpt menu
+const dptButton = document.querySelector('.dpt-cat .dpt-trigger'),
+    dptClass = document.querySelector('.site');
+
+dptButton.addEventListener('click', function() {
+    dptClass.classList.toggle('showdpt')
+})
+
+
+
+
+
+
+
+// product image slider
+var productThumb = new Swiper('.small-image', {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        481: {
+            spaceBetween: 32,
+        }
+    }
+});
+
+var productBig = new Swiper('.big-image', {
+    loop: true,
+    autoHeight: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev', // Corrected the typo here
+    },
+    thumbs: {
+        swiper: productThumb,
+    }
+});
+
+
+
+
+
+//stock product bar width precentage
+var stocks = document.querySelectorAll('.products .stock');
+for (let x = 0; x < stocks.length; x++) {
+    let stock = stocks[x].dataset.stock,
+    available = stocks[x].querySelector('.qty-available').innerHTML,
+    sold = stocks[x].querySelector('.qty-sold').innerHTML,
+    percent = sold*100 / stock;
+
+    stocks[x].querySelector('.available').style.width = percent + '%';
+
+}
